d21bab0be73377a8f40c86a43654795ccf08d191
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-06 06:23:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(name)
   array.each  do |name|
     return "Hello, my name is #{name}."
   end

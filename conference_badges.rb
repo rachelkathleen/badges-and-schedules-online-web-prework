e1bd42536899e2205c5c793469b8731d07974e44
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-06 06:00:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+  #should return a formatted badge (FAILED - 1)
+#batch_badge_creator
+  #should return a list of badge messages (FAILED - 2)
+  #should not hard-code response (FAILED - 3)
+#assign_rooms
+  #should return a list of welcome messages and room assignments (FAILED - 4)
+  #should not hard-code the response (FAILED - 5)
+#printer
+  #should puts the list of badges and room_assignments (FAILED - 6)# Write your code here.

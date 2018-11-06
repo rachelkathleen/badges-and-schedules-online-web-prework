7f97c2c3d650ddddb1db0182a8fbaa0d0d0628ea
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-06 06:51:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,6 +16,12 @@ def assign_rooms(attendees)
   end
 end
 
-def printer(method)
-  print method
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
 end
